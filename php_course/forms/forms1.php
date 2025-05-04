23c33af35b18a4a9c0a068533ff58962a0d48215
Xaralampos Marios <makridhs.xaralampos@gmail.com>
2025-05-04 14:05:51 +0300

    Add files via upload

---
+++ php_course/forms/forms1.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Welcome </title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <form method="get" action="welcome1.php"> <!--we will use the php method GET in the file welcome1.php -->
+            <p>Enter your name: <input type="text" name="name" required></p>
+            <p>Enter your email: <input type="email" name="email" required></p>
+            <p><input type="submit" value="Subscribe"></p>
+        </form>
+<!-- The  post method is the same but your input values doesnt appear in the URL like the get method.
+     If you want to choose post method just change method="post"-->
+    </body>
+</html>
