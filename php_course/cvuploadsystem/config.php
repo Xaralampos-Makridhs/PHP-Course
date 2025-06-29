4cc03fe860155d71675128619e62214a8d596841
Xaralampos Makridhs <makridhs.xaralampos@gmail.com>
2025-06-29 19:54:29 +0300

    Update config.php

--- php_course/cvuploadsystem/config.php
+++ php_course/cvuploadsystem/config.php
@@ -1,7 +1,7 @@
 <?php
     $server="localhost";
     $name="root";
-    $password="xm180605";
+    $password="";//enter your password
     $db="cvupload";
 
     $conn=new mysqli($server,$name,$password,$db);
