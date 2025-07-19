02b82ec8984f3a801145b4ccce4a80f9c79fc89f
Xaralampos Makridhs <makridhs.xaralampos@gmail.com>
2025-07-19 11:42:33 +0300

    Update index.php

--- projects/quiz_system/index.php
+++ projects/quiz_system/index.php
@@ -16,7 +16,7 @@ $questions = $result->fetch_all(MYSQLI_ASSOC);
 
     <form action="submit.php" method="post">
         <?php foreach ($questions as $index => $q): ?>
-            <p>Ερώτηση <?php echo $index + 1; ?>: <strong><?php echo htmlspecialchars($q['question']); ?></strong></p>
+            <p>Question <?php echo $index + 1; ?>: <strong><?php echo htmlspecialchars($q['question']); ?></strong></p>
 
             <label><input type="radio" name="answers[<?php echo $q['id']; ?>]" value="a"> <?php echo htmlspecialchars($q['option_a']); ?></label><br>
             <label><input type="radio" name="answers[<?php echo $q['id']; ?>]" value="b"> <?php echo htmlspecialchars($q['option_b']); ?></label><br>
